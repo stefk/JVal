a91a8c390318b0798a8b8e86b6c44784e8a22210
Stéphane Klein <stephaneklein221@gmail.com>
2015-10-28 11:18:37 +0100

    Add license

--- src/Validator.php
+++ src/Validator.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * This file is part of the JVal package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace JVal;
 
 use JVal\Constraint;
